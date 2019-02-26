eec0c1e1ed8e1a694d8e3ae67971ac514bb4f457
Olusola Ajayi <olusola.ajayi@andela.com>
2019-02-26 10:54:50 +0100

    [Chore-164239014] New react app

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import App from './App';
+import store from './store';
+import './styles/global.scss';
+import '../public/images/favicon.ico';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('index')
+);
